88123a886c947697741c9ce4d81b21c5d6844fcc
Chandrakant-iitbbs <chandrakantgupta681@gmail.com>
2024-09-15 01:15:32 +0530

    Added note context

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import AddNote from './components/AddNote';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
     <>
-
-      <Router>
-        <Routes>
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/signUp" element={<SignUp />} />
-
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Routes>
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signUp" element={<SignUp />} />
+            <Route exact path="/addnote" element={<AddNote />} />
+          </Routes>
+        </Router>
+      </NoteState>
     </>
   );
 }
