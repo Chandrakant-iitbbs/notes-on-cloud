b8ce83017b68b2207cf547adf8fffc6383785081
Chandrakant-iitbbs <chandrakantgupta681@gmail.com>
2024-09-15 02:38:21 +0530

    Created navbar & about component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import Login from './components/Login';
 import SignUp from './components/SignUp';
 import AddNote from './components/AddNote';
 import NoteState from './context/notes/NoteState';
+import Navbar from './components/Navbar';
+import About from './components/About';
 
 function App() {
   return (
     <>
       <NoteState>
         <Router>
+        <Navbar />
           <Routes>
+            <Route exact path="/" element={<About />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/signUp" element={<SignUp />} />
             <Route exact path="/addnote" element={<AddNote />} />
+            
           </Routes>
         </Router>
       </NoteState>
